0b462e9f4ec75b080dc6f240454d168923a194ec
r0ckz <49621441+r0ckz@users.noreply.github.com>
2021-11-20 16:42:50 +0100

    Update wsenable.sh

--- wsenable.sh
+++ wsenable.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Usage: wsenable.sh <hash of torrent>
 # Enable a tracker for a specific torrent by hash
+# Usage: wsenable.sh <hash of torrent>
 xmlrpc https://s1-rutorrent.seedboxws.com/plugins/httprpc/action.php t.multicall $1 '' t.enable= -username=usr00001 -password=PasswordHere123!
